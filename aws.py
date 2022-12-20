c9043b2a1e56eeea10135d275d9b57dc077b4e00
Ashik Kumar <79791821+Ashik9576@users.noreply.github.com>
2022-12-20 10:19:01 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -7,7 +7,7 @@ s3_client = boto3.client('s3',
 aws_access_key_id=access_key,
 aws_secret_access_key = access_secret)
 def s3audio():
-    resp= s3_client.upload_file("Audio\\botaudio.wav",'pace20-dev-all-companies',"8/safety/observations/Bot_recordings/botaudio.wav")
+    resp= s3_client.upload_file("Audio\\botaudio.wav",'pace20-companies',"8/Bot_recordings/boio.wav")
     filelist = glob.glob(os.path.join(dir, "*"))
     for f in filelist:
         os.remove(f)
